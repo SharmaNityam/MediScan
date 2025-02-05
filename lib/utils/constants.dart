1d492c7f1363a8ff3faac44686c82d12c63e68f7
Nityam Sharma <142774674+SharmaNityam@users.noreply.github.com>
2025-02-06 01:35:06 +0530

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,14 @@
+class AppConstants {
+  static const String textsCollection = 'texts';
+  static const String imagesCollection = 'images';
+
+  static const String textAnalysisEndpoint =
+      'Enter-link-here';
+  static const String imageAnalysisEndpoint =
+      'Enter-link-here';
+  static const String followUpQuestionEndpoint =
+      'Enter-link-here';
+
+  static const double borderRadius = 15.0;
+  static const double defaultPadding = 16.0;
+}
